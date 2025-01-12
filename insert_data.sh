1a0926202b00d5bdce30a99fb6e90ba23f2210f9
Daniel Polukchi <danilabodrov746@gmail.com>
2025-01-12 03:48:53 +0000

    Save progress: 562.1

--- insert_data.sh
+++ insert_data.sh
@@ -6,6 +6,7 @@ cat courses.csv | while IFS="," read MAJOR  COURSE
 
 do
 # get major_id
+MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
 #if not found
 #insert major
 #get new major_id
