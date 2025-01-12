5a1e424ac5d95c60168683c3d9f7f4b8311b2362
Daniel Polukchi <danilabodrov746@gmail.com>
2025-01-12 10:26:46 +0000

    Save progress: 760.1

--- insert_data.sh
+++ insert_data.sh
@@ -9,28 +9,28 @@ cat courses_test.csv | while IFS="," read MAJOR COURSE
 do
   if [[ $MAJOR != major ]]
   then
-  # get major_id
-  MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
- 
+      # get major_id
+      MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+    
 
-  # if not found
-  if [[ -z $MAJOR_ID ]]
-  then
-    # insert major
-    INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES ('$MAJOR')")
-    echo $INSERT_MAJOR_RESULT
-    # get new major_id
+      # if not found
+      if [[ -z $MAJOR_ID ]]
+      then
+        # insert major
+        INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES ('$MAJOR')")
+        echo $INSERT_MAJOR_RESULT
+        # get new major_id
 
+      fi
+
+      # get course_id
+
+      # if not found
+
+      # insert course
+
+      # get new course_id
+
+      # insert into majors_courses
   fi
-
-  # get course_id
-
-  # if not found
-
-  # insert course
-
-  # get new course_id
-
-  # insert into majors_courses
-fi
 done
