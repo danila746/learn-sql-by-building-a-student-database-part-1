2f51dbb22b3cf61e40d5490644457e5dbcd5bb12
Daniel Polukchi <danilabodrov746@gmail.com>
2025-01-13 16:25:03 +0000

    Save progress: 853.1

--- insert_data.sh
+++ insert_data.sh
@@ -19,20 +19,24 @@ do
       if [[ $INSERT_MAJOR_RESULT == "INSERT 0 1" ]]
       then
         echo Inserted into majors, $MAJOR
-      fi      
+      fi
+
       # get new major_id
       MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
     fi
 
-
     # get course_id
-COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE' ")
+    COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
+
     # if not found
-if [[ -z $COURSE_ID ]]
-then
-    # insert course
-    # get new course_id
-fi
+    if [[ -z $COURSE_ID ]]
+    then
+      # insert course
+      INSERT_COURSE_RESULT=$($PSQL "INSERT INTO courses(course) VALUES ('$COURSE')")
+      # get new course_id
+
+    fi
+
     # insert into majors_courses
 
   fi
